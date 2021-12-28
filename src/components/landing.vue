de2a8564cec2a781d3214f4999799d26edce30d1
Asif Encoder IT <asif@encoderit.net>
2021-12-29 00:05:27 +0600

    responsive issue fixed.

--- src/components/landing.vue
+++ src/components/landing.vue
@@ -3,44 +3,44 @@
     <section style="position: relative;">
       <img class="img" src="@/assets/Canvas.svg" alt srcset />
       <div class="img-text">
-        <div class="text title">Keep Me Informed!</div>
+        <div class="title">Keep Me Informed!</div>
         <div class="subtitle">Your Artist. Your Experience. Your Piece</div>
       </div>
     </section>
     <section class="register-panel mt-n2">
       <v-container>
         <div class="mx-auto" style="max-width:1086px">
-          <div class="d-flex align-center justify-center mb-16">
+          <div class="d-flex align-center justify-center mb-6 mb-md-16">
             <img class="right-arrow" src="@/assets/point.png" alt />
             <span class="register-hint">Curious? We will send you regularly updates:</span>
           </div>
-          <div class="d-flex justify-space-between mb-8">
+          <div class="d-flex px-4 justify-space-between mb-4 mb-md-8">
             <div class="d-flex align-center">
               <img src="@/assets/headphones.png" alt />
-              <span class="font-base ml-12 white--text">
+              <span class="font-base ml-2 ml-sm-4 ml-md-12 white--text">
                 News around the
                 <span class="primary-text">Roadmap</span>
               </span>
             </div>
-            <div class="d-flex align-center">
+            <div class="d-flex ml-n3 align-center">
               <img src="@/assets/headphones.png" alt />
-              <span class="font-base ml-12 mr-2 white--text">
+              <span class="font-base ml-2 ml-sm-4 ml-md-12 mr-2 white--text">
                 Invites to
                 <span class="primary-text">Our Beta</span>
               </span>
             </div>
           </div>
-          <div class="d-flex justify-space-between mb-8">
+          <div class="d-flex px-4 justify-space-between mb-4 mb-md-8">
             <div class="d-flex align-center">
               <img src="@/assets/headphones.png" alt />
-              <span class="font-base ml-12 white--text">
+              <span class="font-base ml-2 ml-sm-4 ml-md-12 white--text">
                 New
                 <span class="primary-text">Features</span>
               </span>
             </div>
             <div class="d-flex align-center">
               <img src="@/assets/headphones.png" alt />
-              <span class="font-base ml-12 white--text">
+              <span class="font-base ml-2 ml-sm-4 ml-md-12 white--text">
                 Upcoming
                 <span class="primary-text">Specials</span>
               </span>
@@ -51,29 +51,29 @@
           <v-col cols="12" md="5">
             <div class="d-flex align-center">
               <img src="@/assets/headphones.png" alt />
-              <span class="font-base ml-12">News around the Roadmap</span>
+              <span class="font-base ml-2 ml-sm-4 ml-md-12">News around the Roadmap</span>
             </div>
           </v-col>
           <v-col cols="12" md="5">
             <div class="d-flex align-center">
               <img src="@/assets/headphones.png" alt />
-              <span class="font-base ml-12">Invites to Our Beta</span>
+              <span class="font-base ml-2 ml-sm-4 ml-md-12">Invites to Our Beta</span>
             </div>
           </v-col>
           <v-col cols="12" md="5">
             <div class="d-flex align-center">
               <img src="@/assets/headphones.png" alt />
-              <span class="font-base ml-12">New Features</span>
+              <span class="font-base ml-2 ml-sm-4 ml-md-12">New Features</span>
             </div>
           </v-col>
           <v-col cols="12" md="5">
             <div class="d-flex align-center">
               <img src="@/assets/headphones.png" alt />
-              <span class="font-base ml-12">Upcoming Specials</span>
+              <span class="font-base ml-2 ml-sm-4 ml-md-12">Upcoming Specials</span>
             </div>
           </v-col>
         </v-row>-->
-        <div class="text-center mt-16">
+        <div class="text-center mt-8 mt-md-16">
           <v-form class="px-2" ref="registerForm" @submit.prevent="onRegister">
             <v-text-field
               class="mb-5 mx-auto register-field"
@@ -119,7 +119,7 @@
               ></v-checkbox>
             </div>
             <v-btn
-              class="mt-13 mb-11 register-btn"
+              class="mt-5 mb-4 mt-md-13 mb-md-11 register-btn"
               :block="$vuetify.breakpoint.smAndDown"
               type="submit"
               :class="$vuetify.breakpoint.mdAndUp? 'register-btn-width':''"
@@ -248,6 +248,91 @@ $primary-text-color: #EED179;
     color: $primary-text-color;
   }
 }
-
+@media screen and (max-width: 960px) {
+    .font-base{
+    font-size:1rem
+  }
+  .img-text{
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    text-align: center;
+    .title{
+      font-size: 1.5rem !important;
+      color: $primary-text-color;
+      line-height: 2rem;
+    }
+    .subtitle{
+      color: #ffffff;
+      font-size: 1rem;
+    }
+  }
+  .discord{
+  display: flex;
+  align-items: center;
+  max-width: 600px;
+  justify-content: center;
+  .logo{
+    width: 42px!important;
+    margin-right: 1rem;
+    
+  }
+  .text{
+    font-size: 1.75rem;
+    color: $primary-text-color;
+  }
+}
+  .register-panel{
+  background-color: #350045;
+  padding-top: 25px;
+  padding-bottom: 35px;
+  img{
+    width: 28px!important;
+  }
+  .right-arrow{
+    margin-right: 8px;
+  }
+  .register-hint{
+    color: $primary-text-color;
+    font-size:1rem;
+  }
+}
+}
+@media screen and (max-width: 460px) {
+  .font-base{
+    font-size:.75rem
+  }
+  .register-panel{
+      background-color: #350045;
+      padding-top: 25px;
+      padding-bottom: 35px;
+      img{
+        width: 28px!important;
+      }
+      .right-arrow{
+        margin-right: 8px;
+      }
+      .register-hint{
+        color: $primary-text-color;
+        font-size:.75rem;
+    }
+}
+   .discord{
+  display: flex;
+  align-items: center;
+  max-width: 600px;
+  justify-content: center;
+  .logo{
+    width: 32px!important;
+    margin-right: 1rem;
+    
+  }
+  .text{
+    font-size: 1.25rem;
+    color: $primary-text-color;
+  }
+}
+}
 </style>
 
